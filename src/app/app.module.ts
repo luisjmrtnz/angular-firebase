2ec0f9e7f89421ba2b819e4b6e17e78de92aa99b
luisjmartnz <luis_martin_24@hotmail.com>
2017-01-15 15:12:48 -0400

    Setting up Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
+import { CONFIG } from '../environments/firebaseconfig';
+ 
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(CONFIG)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/firebaseconfig.ts
@@ -0,0 +1,7 @@
+export const CONFIG = {
+    apiKey: "AIzaSyCUZhjjSjo_uROgWZQITq--dwYftAphm9w",
+    authDomain: "angular-fb-f5ff0.firebaseapp.com",
+    databaseURL: "https://angular-fb-f5ff0.firebaseio.com",
+    storageBucket: "angular-fb-f5ff0.appspot.com",
+    messagingSenderId: "830007857676"
+};
